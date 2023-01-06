efda106e481dd0723f1ade62b49b8babdaf8208a
jared-GG747 <jdgib18@gmail.com>
2023-01-05 20:17:43 -0600

    added answer feedback functionality

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,11 @@ let questions = [
     {
         numb: 1,
         question: "Which of these is a valid way to alert a string message?",
-        answer: "window.alert.('message')",
+        answer: "window.alert('message')",
         options: [
             "document.alert('message')",
-            "global.alert('message')"
+            "global.alert('message')",
+            "window.alert('message')"
         ]
     },
     {
@@ -14,6 +15,7 @@ let questions = [
         question: "Which of these is an assignment operator",
         answer: "/=",
         options: [
+            "/=",
             "!==",
             "<="
         ]
@@ -23,6 +25,7 @@ let questions = [
         question: "Which event handler is used to process the Click event?",
         answer: "onclick",
         options: [
+            "onclick",
             "onkeydown",
             "onmouseclick"
         ]
@@ -33,7 +36,9 @@ let questions = [
         answer: "concat()",
         options: [
             "append()",
-            "attach()"
+            "attach()",
+            "concat()"
+
         ]
     },
     {
@@ -42,6 +47,7 @@ let questions = [
         answer: "indexOf()",
         options: [
             "pos()",
+            "indexOf()",
             "position()"
         ]
     }
@@ -53,6 +59,8 @@ const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
+
+const option_list = document.querySelector(".option_list");
 
 // If start quiz button clicked
 start_btn.onclick = () => {
@@ -92,20 +100,51 @@ next_btn.onclick = () => {
 // getting questions and options from array
 function showQuestions(index) {
     const que_text = document.querySelector(".que_text");
-    const option_list = document.querySelector(".option_list");
     var que_tag = '<span>' + questions[index].numb + ". " + questions[index].question + '</span>';
     var option_tag = '<div class="option">' + questions[index].options[0] + '<span></span></div>'
-                    + '<div class="option">' + questions[index].options[1] + '<span></span></div>';
+                    + '<div class="option">' + questions[index].options[1] + '<span></span></div>' + '<div class="option">' + questions[index].options[2] + '<span></span></div>';
     que_text.innerHTML = que_tag; 
     option_list.innerHTML = option_tag;
+    
     const option = option_list.querySelectorAll(".option");
-    for (let i = 0; i < option_list.length; i++) {
+    for (let i = 0; i < option.length; i++) {
         option[i].setAttribute("onclick", "optionSelected(this)")
     }
 
 };
 
+let tickIcon = '<div class="icon tick"><i class="fa-solid fa-check"></i></div>';
+let crossIcon = '<div class="icon cross"><i class="fa-solid fa-xmark"></i></div>'
+
+function optionSelected(answer) {
+    let userAns = answer.textContent;
+    let correctAns = questions[que_count].answer;
+    let allOptions = option_list.children.length;
+    if (userAns == correctAns) {
+        answer.classList.add("correct");
+        console.log("Answer is correct");
+        answer.insertAdjacentHTML('beforeend', tickIcon);
+    } else {
+        answer.classList.add("incorrect");
+        console.log("Answer is incorrect");
+        answer.insertAdjacentHTML('beforeend', crossIcon);
+
+        // if answers is incorret then automatically selected the correct answer
+        for (let i = 0; i < allOptions; i++) {
+            if (option_list.children[i].textContent == correctAns) {
+                option_list.children[i].setAttribute("class", "option correct");
+                option_list.children[i].insertAdjacentHTML('beforeend', tickIcon);
+            }
+        }
+    }
+    // once user selected disabled all options
+    for (let i = 0; i < allOptions; i++) {
+        option_list.children[i].classList.add("disabled");
+    }
+}
+
 function queCounter(index) {
 const bottom_ques_counter = quiz_box.querySelector(".total_que");
 let totalQuesCountTag = '<span><p>' + que_count + '</p>Of<p>' + questions.length + '</p>Questions</span>';
+bottom_ques_counter.innerHTML = totalQuesCountTag;
 }
